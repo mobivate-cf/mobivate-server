9517846e2a2c8a256c1b5256541673bc160d665d
Chris Kozlowski <kozlowskicd@gmail.com>
2019-07-11 15:25:59 -0700

    twitter oauth working

--- src/app.js
+++ src/app.js
@@ -1,28 +1,69 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
 const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
+
+let trustProxy = false;
+if (process.env.DYNO) {
+  trustProxy = true;
+}
+
+passport.use(new Strategy({
+    consumerKey: process.env.TWITTER_ID,
+    consumerSecret: process.env.TWITTER_PASS,
+    callbackURL: '/oauth/callback',
+    proxy: trustProxy
+  },
+  function(token, tokenSecret, profile, cb) {
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 const app = express();
+app.use(express.static('public'))
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-const errorHandler = require( './middleware/error.js');
-const notFound = require( './middleware/404.js' );
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.get('/', (request, response) => {
+  response.send('index.html');
+});
 
-app.get('/', getHome)
-
-app.use(notFound);
-app.use(errorHandler);
-
-function getHome(request, response, next) {
+app.get('/login', (request, response) => {
   response.sendStatus(200);
-}
+});
+
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+app.get('/oauth/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  (request, response) => {
+    response.redirect('/dashboard');
+  }
+);
+
+app.get('/dashboard', (request, response) => {
+  response.send('Logged in!');
+});
+
+app.get('/logout', (request, response) => {
+  request.session.destroy(err => {
+    response.redirect('/');
+  }); 
+});
 
 module.exports = {
   server: app,
