1a1eb457b793317d00dfb769fb56db088312f922
Jag <jagdeepsingh3939@gmail.com>
2019-07-15 11:25:25 -0700

    updated redirect url to exp://

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ app.get(
 
     // send username, displayname and id to frontend
     // response.send({ userId, userName, userScreenName, photoLink, oAuthToken, oAuthVerifier });
-    response.redirect(`https://exp.host/@melissastock/front-end`);
+    response.redirect(`exp://exp.host/@melissastock/front-end`);
 
     // response.redirect(`https://exp.host/@melissastock/front-end/?username=${userName}`);
   }
