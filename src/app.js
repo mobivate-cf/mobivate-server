e2426422fa35799f3fb6178913d7c5687024e064
Jag <jagdeepsingh3939@gmail.com>
2019-07-15 15:43:10 -0700

    removed sessions

--- src/app.js
+++ src/app.js
@@ -37,10 +37,10 @@ const app = express();
 app.use(express.static('public'));
 app.use(require('morgan')('combined'));
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+// app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 app.get('/login/twitter', passport.authenticate('twitter'), (request, response) => {
   response.send({ hello: 'this is the auth route' });
