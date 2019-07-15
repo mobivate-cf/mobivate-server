2be7bd194d8638312476a8d8b4863e8598f5f049
Jag <jagdeepsingh3939@gmail.com>
2019-07-15 09:44:01 -0700

    testing oauth callback request object

--- src/app.js
+++ src/app.js
@@ -11,15 +11,19 @@ if (process.env.DYNO) {
   trustProxy = true;
 }
 
-passport.use(new Strategy({
-    consumerKey: process.env.TWITTER_ID,
-    consumerSecret: process.env.TWITTER_PASS,
-    callbackURL: '/oauth/callback',
-    proxy: trustProxy
-  },
-  function(token, tokenSecret, profile, cb) {
-    return cb(null, profile);
-  }));
+passport.use(
+  new Strategy(
+    {
+      consumerKey: process.env.TWITTER_ID,
+      consumerSecret: process.env.TWITTER_PASS,
+      callbackURL: '/oauth/callback',
+      proxy: trustProxy,
+    },
+    function(token, tokenSecret, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -30,7 +34,7 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(require('morgan')('combined'));
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
@@ -38,34 +42,27 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveU
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (request, response) => {
-  response.send('index.html');
+app.get('/login/twitter', (request, response) => {
+  passport.authenticate('twitter')(request, response);
 });
 
-app.get('/login', (request, response) => {
-  response.sendStatus(200);
+app.get('/oauth/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), (request, response) => {
+  console.log(request);
+  response.send(request);
+  // response.redirect('https://exp.host/@jagdeepsing_/spike');
 });
-
-app.get('/login/twitter', passport.authenticate('twitter'));
-
-app.get('/oauth/callback',
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  (request, response) => {
-    response.redirect('exp://8q-3ig.kozlowskicd.react-native-spike.exp.direct:80');
-  }
-);
 
 app.get('/dashboard', (request, response) => {
   response.send('Logged in!');
 });
 
 app.get('/logout', (request, response) => {
-  request.session.destroy(err => {
+  request.session.destroy((err) => {
     response.redirect('mobivate://');
-  }); 
+  });
 });
 
 module.exports = {
   server: app,
-  start: (port) => app.listen(port, () => console.log(`Server up on port ${port}`) ),
+  start: (port) => app.listen(port, () => console.log(`Server up on port ${port}`)),
 };
