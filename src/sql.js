2a0519b024c00a8197accebfdba2134ab534a269
rebecca-pete <rebecca@rebeccapeterson.us>
2019-07-16 13:58:29 -0700

    update sql.js file

--- src/sql.js
+++ src/sql.js
@@ -1,5 +1,3 @@
-const sql = {
+module.exports = {
   test: `SELECT * FROM users JOIN goals ON (users.user_id = goals.goal_user_id) JOIN progress ON (users.user_id = progress.progress_user_id)`
 }
-
-export default sql;
