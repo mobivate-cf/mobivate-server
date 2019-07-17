aa97950be227e44233ea0a90fa1704b40f5592e5
rebecca-pete <rebecca@rebeccapeterson.us>
2019-07-17 13:52:13 -0700

    I can spell process

--- src/oauth-helpers.js
+++ src/oauth-helpers.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const jsonWebToken = require('jsonwebtoken');
 
 const SECRET = process.env.JSONWEBTOKEN_SECRET;
-const SALTS = proccess.env.SALTS;
+const SALTS = process.env.SALTS;
 
 module.exports = {
 
