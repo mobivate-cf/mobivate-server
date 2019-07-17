b6f6db9d3850d78e8d246adf788d55475d5446b6
Chris Kozlowski <kozlowskicd@gmail.com>
2019-07-16 17:56:41 -0700

    update test sql

--- src/sql.js
+++ src/sql.js
@@ -1,5 +1,5 @@
 module.exports = sql = {
-  test: `SELECT * FROM users JOIN goals ON (users.user_id = goals.goal_user_id) JOIN progress ON (users.user_id = progress.progress_user_id)`,
+  test: `SELECT * FROM users LEFT JOIN progress ON (users.user_id = progress.progress_user_id) LEFT JOIN goals ON (progress.progress_goal_id = goals.goal_id)`,
   createGoal: `INSERT INTO goals (goal_user_id, goal_name, goal_start, goal_end, frequency) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
   createProgress: `INSERT INTO progress (progress_user_id, progress_goal_id, streak, num_of_completed_goals, num_of_total_goals, next_due_date) VALUES ($1, $2, 0, 0, 0, 0)`,
 }
